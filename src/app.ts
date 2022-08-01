d2a94f0883872f36ba8e4b03885107d39119eb40
Matheus <stvgamer74@gmail.com>
2022-08-01 18:00:32 -0300

    feat: creating all routers(auth, orders, products, users) files

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import route from './routers';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', route.products);
 
 export default app;
---
+++ src/routers/index.ts
@@ -0,0 +1,6 @@
+import auth from './auth.router';
+import order from './orders.router';
+import products from './products.router';
+import users from './users.router';
+
+export default { auth, order, products, users };
