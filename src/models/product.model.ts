6839467ed85b164a831d1d1eada80a53b615b123
Matheus <stvgamer74@gmail.com>
2022-08-02 21:05:21 -0300

    refactor: replace public for private with attribute connection in product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,7 +2,7 @@ import { Pool, ResultSetHeader } from 'mysql2/promise';
 import IProduct from '../interfaces/product.interface';
 
 export default class ProductModel {
-  public connection: Pool;
+  private connection: Pool;
 
   constructor(connection: Pool) {
     this.connection = connection;
