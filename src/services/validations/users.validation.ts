4c027b467c92fc6835aad55d25db5f329e744632
Matheus <stvgamer74@gmail.com>
2022-08-02 21:06:12 -0300

    feat: create user validatation

---
+++ src/services/validations/users.validation.ts
@@ -0,0 +1,19 @@
+import Joi, { ValidationResult } from 'joi';
+import IUser from '../../interfaces/user.interface';
+
+export const validateCreate = (user: IUser): ValidationResult => {
+  const schema = Joi.object({
+    username: Joi.string().required(),
+    classe: Joi.string().required(),
+    level: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+
+  const { error, value } = schema.validate(user);
+
+  if (error) throw error;
+
+  return value;
+};
+
+export const a = 'hello world';
