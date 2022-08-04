77cb6e583ed2e4860f4925ef030b7230acd52260
Matheus <stvgamer74@gmail.com>
2022-08-04 06:45:29 -0300

    feat: add validation for products string type and string length

--- src/services/validations/products.validation.ts
+++ src/services/validations/products.validation.ts
@@ -1,15 +1,25 @@
-import Joi, { ValidationResult } from 'joi';
+import Joi, { ValidationError, ValidationResult } from 'joi';
 import IProduct from '../../interfaces/product.interface';
+import CustomError from '../../utils/customError.util';
+
+const joiCustomException = (error: ValidationError) => {
+  const { type, message } = error.details[0];
+
+  if (type === 'string.base' || type === 'string.min') {
+    return new CustomError('UnprocessableEntity', message);
+  }
+  return error;
+};
 
 export const validateCreateProduct = (product: IProduct): ValidationResult => {
   const schema = Joi.object({
-    name: Joi.string().required(),
-    amount: Joi.string().required(),
+    name: Joi.string().min(3).required(),
+    amount: Joi.string().min(3).required(),
   });
 
   const { error, value } = schema.validate(product);
 
-  if (error) throw error;
+  if (error) throw joiCustomException(error);
 
   return value;
 };
