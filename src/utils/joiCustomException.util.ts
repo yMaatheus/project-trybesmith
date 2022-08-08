e0931e116af6b9f3af0c59ae120eaac29a2d008b
Matheus <stvgamer74@gmail.com>
2022-08-07 23:37:47 -0300

    refactor: add errorNames object

--- src/utils/joiCustomException.util.ts
+++ src/utils/joiCustomException.util.ts
@@ -1,12 +1,24 @@
 import { ValidationError } from 'joi';
 import CustomError from './customError.util';
 
+type Names = {
+  [key: string]: string
+};
+
+const errorName: Names = {
+  'string.base': 'UnprocessableEntity',
+  'string.min': 'UnprocessableEntity',
+  'number.base': 'UnprocessableEntity',
+  'number.min': 'UnprocessableEntity',
+  'array.base': 'UnprocessableEntity',
+  'array.includesRequiredUnknowns': 'UnprocessableEntity',
+};
+
 export default (error: ValidationError) => {
   const { type, message } = error.details[0];
 
-  if (type === 'string.base' || type === 'string.min' 
-  || type === 'number.base' || type === 'number.min') {
-    return new CustomError('UnprocessableEntity', message);
-  }
+  const name = errorName[type];
+
+  if (name) return new CustomError(name, message);
   return error;
 };
