94aba2de7692e79f0e20cec0c5201a79537ad685
Matheus <stvgamer74@gmail.com>
2022-12-30 20:30:52 -0300

    feat: :sparkles: implements swagger documentation and cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import doc from '../swagger.json';
+
 import handleErrors from './middlewares/handleErrors.middleware';
 
 import productsRouter from './routers/products.router';
@@ -9,10 +13,13 @@ import ordersRouter from './routers/orders.router';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/products', productsRouter);
 app.use(usersRouter);
 app.use('/orders', ordersRouter);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(doc));
 
 app.use(handleErrors);
 
