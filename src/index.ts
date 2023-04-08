a095461f99e18b011ced98e6fa50af9987f283f5
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-08 20:39:20 -0300

    feat: :sparkles: add get env PORT in to listen server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv';
 import app from './app';
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
